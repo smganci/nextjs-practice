85712f19d24716ba314110bc45b2fe126fd0bcff
Sarah Ganci <sarahganci@gmail.com>
2023-10-19 23:36:56 -0700

    data fetching lesson

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,8 +1,20 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
 
-export default function Home() {
+//get static props allows us to make a static call to get our data and return that as part of props in the return object
+// when a page includes an export of getStaticProps, then it automatically knows to render and save at build time and build using the rpops resulting from getStaticProps
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  // the result of getStaticProps .props is passed into the functions on the same page?
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+export default function Home({allPostsData}) {
   return (
     <Layout home>
       <Head>
@@ -14,6 +26,21 @@ export default function Home() {
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+      </section>
+            {/* Add this <section> tag below the existing <section> tag */}
+            <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
